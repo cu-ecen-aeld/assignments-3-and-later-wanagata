67ad7212359df9c9574060ab05a8a7816a640366
wanagata <newanagata@gmail.com>
2025-08-23 22:32:49 +0700

    try to run manual-linux script for kernel building after implement TODO

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,21 +35,21 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
-    # deep clearn kernel 
-    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
-    # QEMU virt machine (defconfig)
-    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
-    # Build the kernel (vmlinux / Image)
-    make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
-    # Build any kernel modules
-    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
-    # Build the devicetree
-    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
+    echo "[Script] deep clean kernel"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo "[Script] QEMU virt machine (defconfig)"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    echo "[Script] Build the kernel (vmlinux / Image)"
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    echo "[Script] Build any kernel modules"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    echo "[Script] Build the devicetree"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
-echo "Adding the Image in outdir"
+echo "[Script] Adding the Image in outdir"
 
-echo "Creating the staging directory for the root filesystem"
+echo "[Script] Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
@@ -66,25 +66,46 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    echo "[Script] Configure busybox"
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+echo "[Script] Make and install busybox"
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 
-echo "Library dependencies"
+echo "[Script] Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-
+echo "[Script] Add library dependencies to rootfs"
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 # TODO: Make device nodes
+echo "[Script] Make device nodes"
+mkdir -p ${OUTDIR}/rootfs/dev
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 622 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+echo "[Script] Clean and build the writer utility"
+cd ${FINDER_APP_DIR}
+make clean; make CROSS_COMPILE=${CROSS_COMPILE} -o writer writer.c
 
 # TODO: Copy the finder related scripts and executables to the /home directory
+echo "[Script] Copy the finder related scripts and executables to the /home directory"
 # on the target rootfs
-
+cp writer ${OUTDIR}/rootfs
+cp finder.sh ${OUTDIR}/rootfs
 # TODO: Chown the root directory
-
+echo "[Script] Chown the root directory"
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
+echo "[Script] Create initramfs.cpio.gz"
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov -- owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f initramfs.cpio
