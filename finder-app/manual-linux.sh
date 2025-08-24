7135057d1918cda8a9adf8731ac7b3291ad85a2b
wanagata <newanagata@gmail.com>
2025-08-24 12:57:09 +0700

    can run and build kernel. have full tested and success

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -40,7 +40,9 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "[Script] QEMU virt machine (defconfig)"
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     echo "[Script] Build the kernel (vmlinux / Image)"
-    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    #$(nproc) → automatically uses all CPU cores.
+    #-j4 limits to 4 cores; 
     echo "[Script] Build any kernel modules"
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
     echo "[Script] Build the devicetree"
@@ -48,7 +50,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 fi
 
 echo "[Script] Adding the Image in outdir"
-
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 echo "[Script] Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -58,6 +60,9 @@ then
 fi
 
 # TODO: Create necessary base directories
+echo "[Script] Create necessary base directories"
+mkdir -p ${OUTDIR}/rootfs/{bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,var/log,usr/{bin,lib,sbin}}
+tree ${OUTDIR}/rootfs
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -75,15 +80,52 @@ fi
 echo "[Script] Make and install busybox"
 make distclean
 make defconfig
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
 echo "[Script] Library dependencies"
+cd ${OUTDIR}/rootfs
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 echo "[Script] Add library dependencies to rootfs"
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+# Get the sysroot path
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+if [ ! -d "$SYSROOT" ]; then
+    echo "Error: Sysroot not found at $SYSROOT"
+    exit 1
+fi
+
+echo "Sysroot path: $SYSROOT"
+# Copy the necessary libraries
+echo "[Script] Copying necessary libraries"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library" | sed 's/.*\[\(.*\)\]/\1/' | while read lib; do
+    echo "Copying library: $lib"
+    # Find the library in sysroot
+    if [ -f "${SYSROOT}/lib64/$lib" ]; then
+        cp ${SYSROOT}/lib64/$lib ${OUTDIR}/rootfs/lib64/
+    elif [ -f "${SYSROOT}/lib/$lib" ]; then
+        cp ${SYSROOT}/lib/$lib ${OUTDIR}/rootfs/lib/
+    else
+        echo "Warning: Library $lib not found in sysroot"
+    fi
+done
+
+# Copy the program interpreter
+echo "[Script] Copy the program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter" | sed 's/.*\[\(.*\)\].*/\1/' | while read interpreter; do
+    INTERPRETER_FILE=$(basename "$interpreter")
+    echo "[Script] Copying interpreter: $INTERPRETER_FILE"
+    interpreter_path=$(find "$SYSROOT" -type f -name "$INTERPRETER_FILE" | head -n 1)
+    if [ -n "$interpreter_path" ]; then
+        cp "$interpreter_path" "${OUTDIR}/rootfs/lib/"
+        echo "Copied interpreter from $interpreter_path to ${OUTDIR}/rootfs/lib/"
+    else
+        echo "Warning: Interpreter $interpreter not found"
+    fi
+done
+
+
 # TODO: Make device nodes
 echo "[Script] Make device nodes"
 mkdir -p ${OUTDIR}/rootfs/dev
@@ -93,13 +135,16 @@ sudo mknod -m 622 ${OUTDIR}/rootfs/dev/console c 5 1
 # TODO: Clean and build the writer utility
 echo "[Script] Clean and build the writer utility"
 cd ${FINDER_APP_DIR}
-make clean; make CROSS_COMPILE=${CROSS_COMPILE} -o writer writer.c
+make clean; make writer CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 echo "[Script] Copy the finder related scripts and executables to the /home directory"
 # on the target rootfs
-cp writer ${OUTDIR}/rootfs
-cp finder.sh ${OUTDIR}/rootfs
+cp writer ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home/
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp -r ../conf ${OUTDIR}/rootfs/home/
 # TODO: Chown the root directory
 echo "[Script] Chown the root directory"
 cd ${OUTDIR}/rootfs
@@ -107,5 +152,5 @@ sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
 echo "[Script] Create initramfs.cpio.gz"
 cd "$OUTDIR/rootfs"
-find . | cpio -H newc -ov -- owner root:root > ${OUTDIR}/initramfs.cpio
-gzip -f initramfs.cpio
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
